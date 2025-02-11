aab1cca9a6ed4f5c63017c93899a9905aa98de3e
andri <andriniaina@gmail.com>
2025-02-11 11:30:20 +0100

    x

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 # build
+# dotnet build src/ImGui.NET -o release/x64 -c Debug -a x64
 docker build --pull -t cross-build-arm64 -f Dockerfile.cross-build-x64-arm64 .
-docker run --rm -v $(pwd)/src:/source -v $(pwd)/deps:/deps -w /source cross-build-arm64 dotnet publish ImGui.NET.SampleProgram/ImGui.NET.SampleProgram.csproj -a arm64 -c Release -o bin-arm64 -p:SysRoot=/crossrootfs/arm64 -p:LinkerFlavor=lld  && \
-rsync -rv src/bin-arm64 root@192.168.0.10:~/ImGui.NET
+docker run --rm -v $(pwd)/src:/source -v $(pwd)/release:/release -v $(pwd)/deps:/deps -w /source cross-build-arm64 dotnet publish ImGui.NET.SampleProgram/ImGui.NET.SampleProgram.csproj -a arm64 -c Release -o ../release/arm64 -p:SysRoot=/crossrootfs/arm64 -p:LinkerFlavor=lld  && \
+rsync -rv src/bin-arm64 root@$KNULLI:~/ImGui.NET
